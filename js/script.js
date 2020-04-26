01d7116a11062d5c964a60131b6a38d52e69c15f
Hannah Leland <dev@hleland.com>
2020-04-25 22:53:20 -0500

    swapped social media icons and links, made message input a textarea

--- js/script.js
+++ js/script.js
@@ -2,74 +2,74 @@
 // form validation
 //
 
-document.getElementById("submit_contact").onclick = validate;
+document.getElementById("submit-contact").onclick = validate;
 
 async function validate() {
 
   var screenwidth = window.innerWidth || document.documentElement.clientWidth;
 
   // email input
-  const email_response = await isEmailValid(document.getElementById("email_input").value);
+  const email_response = await isEmailValid(document.getElementById("email-input").value);
   if (!email_response) {
-    document.getElementById("email_input").style.backgroundColor = "#ffd3d3";
-    document.getElementById("email_label").innerHTML = "Please enter valid email address. <p>Example: address@site.com</p>";
-    document.getElementById("email_label").style.color = "#b30000";
+    document.getElementById("email-input").style.backgroundColor = "#ffd3d3";
+    document.getElementById("email-label").innerHTML = "Please enter valid email address. <p>Example: address@site.com</p>";
+    document.getElementById("email-label").style.color = "#b30000";
 
     if(screenwidth > 1025) {
-      document.getElementById("email_label").style.cssFloat = "none";
-      document.getElementById("email_label").style.width = "30em";
+      document.getElementById("email-label").style.cssFloat = "none";
+      document.getElementById("email-label").style.width = "30em";
     }
     
   } else {
-    document.getElementById("email_input").style.backgroundColor = "#eaebee";
-    document.getElementById("email_label").innerHTML = "Email - Great!";
-    document.getElementById("email_label").style.color = "#061F46";
+    document.getElementById("email-input").style.backgroundColor = "#eaebee";
+    document.getElementById("email-label").innerHTML = "Email - Great!";
+    document.getElementById("email-label").style.color = "#061F46";
 
     if(screenwidth > 1025) {
-      document.getElementById("email_label").style.cssFloat = "left";
-      document.getElementById("email_label").style.width = "12em";
+      document.getElementById("email-label").style.cssFloat = "left";
+      document.getElementById("email-label").style.width = "12em";
     }
   }
 
   // name input
-  if (!(isValuePresent(document.getElementById("name_input").value))) {
-    document.getElementById("name_input").style.backgroundColor = "#ffd3d3";
-    document.getElementById("name_label").innerHTML = "Please enter your name.";
-    document.getElementById("name_label").style.color = "#b30000";
+  if (!(isValuePresent(document.getElementById("name-input").value))) {
+    document.getElementById("name-input").style.backgroundColor = "#ffd3d3";
+    document.getElementById("name-label").innerHTML = "Please enter your name.";
+    document.getElementById("name-label").style.color = "#b30000";
 
     if(screenwidth > 1025) {
-      document.getElementById("name_label").style.cssFloat = "none";
-      document.getElementById("name_label").style.width = "30em";
+      document.getElementById("name-label").style.cssFloat = "none";
+      document.getElementById("name-label").style.width = "30em";
     }
   } else {
-    document.getElementById("name_input").style.backgroundColor = "#eaebee";
-    document.getElementById("name_label").innerHTML = "Name - Great!";
-    document.getElementById("name_label").style.color = "#061F46";
+    document.getElementById("name-input").style.backgroundColor = "#eaebee";
+    document.getElementById("name-label").innerHTML = "Name - Great!";
+    document.getElementById("name-label").style.color = "#061F46";
 
     if(screenwidth > 1025) {
-      document.getElementById("name_label").style.cssFloat = "left";
-      document.getElementById("name_label").style.width = "12em";
+      document.getElementById("name-label").style.cssFloat = "left";
+      document.getElementById("name-label").style.width = "12em";
     }
   }
 
   // message input
-  if(!(isValuePresent(document.getElementById("message_input").value))) {
-    document.getElementById("message_input").style.backgroundColor = "#ffd3d3";
-    document.getElementById("message_label").innerHTML = "Please enter a message";
-    document.getElementById("message_label").style.color = "#b30000";
+  if(!(isValuePresent(document.getElementById("message-input").value))) {
+    document.getElementById("message-input").style.backgroundColor = "#ffd3d3";
+    document.getElementById("message-label").innerHTML = "Please enter a message";
+    document.getElementById("message-label").style.color = "#b30000";
 
     if(screenwidth > 1025) {
-      document.getElementById("message_label").style.cssFloat = "none";
-      document.getElementById("message_label").style.width = "30em";
+      document.getElementById("message-label").style.cssFloat = "none";
+      document.getElementById("message-label").style.width = "30em";
     }
   } else {
-    document.getElementById("message_input").style.backgroundColor = "#eaebee";
-    document.getElementById("message_label").innerHTML = "Message - Great!";
-    document.getElementById("message_label").style.color = "#061F46";
+    document.getElementById("message-input").style.backgroundColor = "#eaebee";
+    document.getElementById("message-label").innerHTML = "Message - Great!";
+    document.getElementById("message-label").style.color = "#061F46";
 
     if(screenwidth > 1025) {
-      document.getElementById("message_label").style.cssFloat = "left";
-      document.getElementById("message_label").style.width = "12em";
+      document.getElementById("message-label").style.cssFloat = "left";
+      document.getElementById("message-label").style.width = "12em";
     }
   }
 }
